390d84fda9893c024fa653d7b51d2dcb4edb95bf
Gary Helmling <gary_helmling@trendmicro.com>
2011-05-26 11:50:03 -0700

    Fix tab usage

--- test/jython/dbtest/hbase/__init__.py
+++ test/jython/dbtest/hbase/__init__.py
@@ -3,20 +3,23 @@ from org.apache.hadoop.hbase import HBaseTestingUtility
 class HBaseContext(HBaseTestingUtility):
     def __init__(self):
         super(HBaseContext, self).__init__()
-        self.setName('beeno')
 
-        if self.conf.get('test.build.data') is None:
-            self.conf.set('test.build.data', '/tmp/beeno')
+        #if self.conf.get('test.build.data') is None:
+        #    self.conf.set('test.build.data', '/tmp/beeno')
 
         self.running = False
 
     def setUp(self):
         if not self.running:
             self.running = True
-            HBaseTestingUtility.startMiniCluster(self)
+            self.startMiniCluster()
 
 
     def tearDown(self):
         if self.running:
             self.running = False
-            HBaseTestingUtility.shutdownMiniCluster(self)
+            self.shutdownMiniCluster()
+
+    def waitFor(self, tableName, timeout):
+        if self.running:
+            self.waitTableAvailable(tableName, timeout)
